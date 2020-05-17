ee2fbe6bd1ee1f4a4c184285723f6b117a3a5fad
Nathaniel Copeland <nathanielc510@gmail.com>
2020-05-16 17:20:53 -0700

    Added first drafts of backend database and monogo class

---
+++ src/backend.py
@@ -0,0 +1,83 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+import random
+import string
+from mongo import SongBank
+
+
+app = Flask(__name__)
+
+#CORS stands for Cross Origin Requests.
+CORS(app) #Here we'll allow requests coming from any domain. Not recommended for production environment.
+
+songs = { 
+    "song_list" :
+    [
+        {  
+            "name" : "Brown Eyed Girl",
+            "artist" : "Van Morrison",
+            "key" : "F major",
+            "bpm" : "120"
+        },
+        {  
+            "name" : "Yellow Submarine",
+            "artist" : "The Beatles",
+            "key" : "G Major",
+            "bpm" : "112"
+        },
+        {  
+            "name" : "The Twist",
+            "artist" : "Chubby Checker",
+            "key" : "E Major",
+            "bpm" : "157"
+        },
+        {  
+            "name" : "Smooth",
+            "artist" : "Santana",
+            "key" : "A Major",
+            "bpm" : "116"
+        },
+        {  
+            "name" : "Uptown Funk",
+            "artist" : "Mark Ronson",
+            "key" : "C Major",
+            "bpm" : "115"
+        }    
+    ]
+}
+
+@app.route('/')
+def hello_world():
+    return 'Welcome to Song Bank!'
+
+@app.route('/songs', methods=['GET', 'POST'])
+def get_songs():
+    if request.method == 'GET':
+        search_songname = request.args.get('name')
+        search_artist = request.args.get('artist')
+        search_key = request.args.get('key')
+        search_bpm = request.args.get('bpm')
+        if search_songname:
+            songs = SongBank().find_by_name(search_songname)  
+        elif search_artist :
+            songs = SongBank().find_by_artist(search_artist)
+        elif search_key :
+            songs = SongBank().find_by_key(search_key)
+        elif search_artist :
+            songs = SongBank().find_by_bpm(search_bpm)
+        else :
+            songs = SongBank().find_all()
+        return {"song_list": songs}
+    elif request.method == 'POST':
+        songToAdd = request.get_json()
+        newSong = SongBank(songToAdd)
+        newSong.save()
+        resp = jsonify(newSong), 201
+        return resp
+
+# def generate_id():
+#     lettersAndDigits = string.ascii_letters + string.digits
+#     return ''.join((random.choice(lettersAndDigits) for i in range(6)))
+   
