096eab65aebf9d7191c5a3d79b5333df2961c0f0
Nathaniel Copeland <35314144+nattiecopeland@users.noreply.github.com>
2020-06-02 21:05:09 -0700

    Report and Search ID Backend

---
+++ Form.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react'
+//import Table from "./Table";
+import axios from 'axios';
+import {Redirect} from "react-router-dom";
+
+class Form extends Component {
+   state = {
+      Artist: '',
+      Name: '',
+      Key: '',
+      BPM: '',
+      username: '',
+      logged_in: false
+      reported: false
+   }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            Artist: '',
+            Name: '',
+            Key: '',
+            BPM: '',
+            username: props.username,
+            logged_in: (props.username!=='')
+        }
+    }
+
+
+   handleChange = event => {
+      const {name, value} = event.target
+
+      this.setState({
+         [name]: value,
+      })
+   }
+   submitForm = () => {
+      this.handleSubmit(this.state)
+      this.setState(this.initialState)
+   }
+
+   makePostCall(song) {
+      return axios.post('http://localhost:5000/songs', song)
+       .then(function (response) {
+         console.log(response);
+         return response;
+      })
+      .catch(function (error) {
+       console.log(error);
+     });
+   }
+   
+   handleSubmit = song => {
+      this.makePostCall(song)
+   }
+   
+
+   render() {
+      const { Artist, Name, Key, BPM } = this.state;
+
+      if(!this.state.logged_in)
+          return(<Redirect to='/' />)
+      else
+          return (
+             <form>
+                <h1>Add a song to the Database with this form!</h1>
+                <label htmlFor="Artist">Artist</label>
+                <input
+                   type="text"
+                   name="Artist"
+                   id="Artist"
+                   value={Artist}
+                   onChange={this.handleChange} />
+                <label htmlFor="Name">Name</label>
+                <input
+                   type="text"
+                   name="Name"
+                   id="Name"
+                   value={Name}
+                   onChange={this.handleChange} />
+                <label htmlFor="Key">Key</label>
+                <input
+                   type="text"
+                   name="Key"
+                   id="Key"
+                   value={Key}
+                   onChange={this.handleChange} />
+                <label htmlFor="BPM">BPM</label>
+                <input
+                   type="text"
+                   name="BPM"
+                   id="BPM"
+                   value={BPM}
+                   onChange={this.handleChange} />
+                <input type="button" value="Submit" onClick={this.submitForm} />
+             </form>
+          );
+   }
+
+}
+
+export default Form;
---
+++ mongo.py
@@ -0,0 +1,106 @@
+import pymongo
+from bson import ObjectId
+
+class Model(dict):
+    """
+    A simple model that wraps mongodb document
+    """
+    __getattr__ = dict.get
+    __delattr__ = dict.__delitem__
+    __setattr__ = dict.__setitem__
+
+    def save(self):
+        if not self._id:
+            self.collection.insert(self)
+        else:
+            self.collection.update(
+                { "_id": ObjectId(self._id) }, self)
+        self._id = str(self._id)
+
+    def reload(self):
+        if self._id:
+            result = self.collection.find_one({"_id": ObjectId(self._id)})
+            if result :
+                self.update(result)
+                self._id = str(self._id)
+                return True
+        return False
+
+    def remove(self):
+        if self._id:
+            resp = self.collection.remove({"_id": ObjectId(self._id)})
+            self.clear()
+            return resp
+
+
+class UserBank(Model):
+    db_client = pymongo.MongoClient('localhost', 27017)
+    collection = db_client["users"]["user_list"]
+
+    def find_all(self):
+       users = list(self.collection.find())
+       for user in users:
+           user["_id"] = str(user["_id"])
+       return users
+
+    def find_by_username(self, username):
+       users = list(self.collection.find({"username": username}))
+       for user in users:
+          user["_id"] = str(user["_id"])
+       return users
+
+class SongBank(Model):
+    db_client = pymongo.MongoClient('localhost', 27017)
+    collection = db_client["songs"]["song_list"]
+
+    def find_all(self):
+        songs = list(self.collection.find())
+        for song in songs:
+            song["_id"] = str(song["_id"])
+        return songs
+
+    def find_by_name(self, name):
+        songs = list(self.collection.find({"Name": name}))
+        for song in songs:
+            song["_id"] = str(song["_id"])
+        return songs
+
+    def find_by_artist(self, artist):
+        songs = list(self.collection.find({"Artist": artist}))
+        for song in songs:
+            song["_id"] = str(song["_id"])
+        return songs
+
+    def find_by_key(self, key):
+        songs = list(self.collection.find({"Key": key}))
+        for song in songs:
+            song["_id"] = str(song["_id"])
+        return songs
+
+    def find_by_bpm(self, bpm):
+        songs = list(self.collection.find({"BPM": bpm}))
+        for song in songs:
+            song["_id"] = str(song["_id"])
+        return songs
+
+    def find_by_reported(self):
+        songs = list(self.collection.find({"reported": True}))
+        for song in songs:
+            song["_id"] = str(song["_id"])
+        return songs
+
+    def find_by_ids(self, ids: list):
+        songs = list()
+        for songid in ids:
+            song = self.collection.findOne({"_id": songid})
+            songs.append(song)    
+        for song in songs:
+            song["_id"] = str(song["_id"])
+        return songs
+
+    def report_song(seld, songid):
+        song = self.collection.findOne({"_id": songid})
+        if song:
+            song["reported"] = True;
+        return song;
+        
